52eec7d4d4c4520d4df26936007580100ff09611
Kaushik <kaushik20004@gmail.com>
2024-07-19 16:53:24 +0800

    express js server working!

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
 import OpenAI from "openai";
+import express from "express";
+const app = express();
+app.use(express.json());
+// const express = require("express");
+// const app=express();
+
+// app.use(express.json());
 
 const openai = new OpenAI();
 
-const runPrompt = async () => {
-  const salary_amount = 50000;
-  const expenses = "rent, groceries, and utilities";
+const runPrompt = async (prompt, salary, expenses) => {
 
-  const prompt = `
-    Give me some financial assistance on how to save money. My salary is ${salary_amount}. My only expenses are ${expenses}.
-    Please return the advice in a parsable JSON format using the below JSON format:
-    {
-        "Financial Advice 1": "Your advice here for 1",
-        "Financial Advice 2": "Your advice here for 2",
-    }`;
 
   const response = await openai.chat.completions.create({
     model: "gpt-4o-mini-2024-07-18", // To select the model you want to use
     messages: [
       {
         role: "system",
-        content: "You are a useful Financial Assistant.",
+        content: "You are a useful Financial Assistant. If you receive any prompts that are not related to personal finance please reply saying Please prompt me based on personal finance",
+      },
+      {
+        role: "system",
+        content: `Please make use of the Users details that you are giving financial assistance to. Their Salary ${salary}, expenses: ${expenses}`
       },
       {
         role: "user",
@@ -32,7 +34,32 @@ const runPrompt = async () => {
 
 //   const parsableJSONResponse = response.data.choice[0].text;
 //   const parsedResponse = JSON.parse(parsableJSONResponse);
-  console.log(response);
+return response
 };
 
-runPrompt();
+
+
+app.get('/getFinancial_Assistance', async (req, res) => {
+  const salary = req.body.salary;
+  const expenses = req.body.expenses;
+  const userPrompt = req.body.prompt;
+
+  const api_response = await runPrompt(userPrompt, salary, expenses); // calling OpenAI API
+
+  console.log(api_response.choices[0].message);
+  res.send(api_response.choices[0].message);
+
+
+});
+
+
+app.listen(3000, () => {
+
+  console.log("server started...")
+
+})
+
+
+
+
+
